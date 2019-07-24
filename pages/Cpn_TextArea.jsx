347bcb47b450fddb0bc22d73aa1fa4502ef365dd
zrx <346669129@qq.com>
2019-07-24 11:16:42 +0800

    react

--- pages/Cpn_TextArea.jsx
+++ pages/Cpn_TextArea.jsx
@@ -6,6 +6,8 @@ action="#" 表示 本表单提交给本页自己.
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
+
 
 import objStore from '../store/objStore.js'
 import * as moduleAction from '../store/fnCreateObjAction.js'
@@ -31,40 +33,30 @@ export default class Cpn_TextArea extends React.Component {
                        }}
                 />
 
-                <form action="#"
-                      method="get"
-                      // onSubmit={() => { //此方式被验证为不可行.
-                      //     moduleMyfunc.fn_发送命令书_获取切片中的词头数组()
-                      //     /*注意!!! 这里一定要重新获取切片中的词头!!
-                      //     因为你这里的词头数组, 是页面渲染后, 再来用按钮手动从state中拿到的,
-                      //     所以并非是页面渲染前的state中的原始数据,
-                      //     所以当本表单被提交后, 后天拿到的state中的数据都会被清空!
-                      //     所以你必须在表单提交后, 再次来获取这个词头数组, 才能保持页面上这块数据的永远显示.
-                      //     */
-                      // }}
-                >
+                {/*多行文本框, 存放已选出来的句子*/}
+                <textarea rows="10" cols="50"
+                          name={'area_StrSelected'}
+                          value={this.state.str_textArea}
+                          onChange={() => {
+                              moduleMyfunc.fn_发送命令书_可编辑str_textArea(event.target.value)
+                          }}/>
+                <br/>
 
-                    {/*多行文本框, 存放已选出来的句子*/}
-                    <textarea rows="10" cols="50"
-                              name={'area_StrSelected'}
-                              value={this.state.str_textArea}
-                              onChange={() => {
-                                  moduleMyfunc.fn_发送命令书_可编辑str_textArea(event.target.value)
-                              }}/>
-                    <br/>
+                {/*按钮:清空文本框*/}
+                <input type="button"
+                       value={'清空'}
+                       onClick={() => {
+                           moduleMyfunc.fn_发送命令书_清空str_textArea()
+                       }}/>
+                &nbsp;
 
-                    {/*按钮:清空文本框*/}
-                    <input type="button"
-                           value={'清空'}
-                           onClick={() => {
-                               moduleMyfunc.fn_发送命令书_清空str_textArea()
-                           }}/>
-                    &nbsp;
+                {/*按钮:保存文本框中的值到电脑文件*/}
+                <input type="button"
+                       value="让服务器保存文件到电脑上"
+                       onClick={()=>{this.fn_sendTextareaContent_ByAxios(this.state.str_textArea)}}
+                />
 
-                    {/*按钮:保存文本框中的值到电脑文件*/}
-                    <input type="submit" value="让服务器保存文件到电脑上"/>
 
-                </form>
                 <hr/>
 
             </React.Fragment>
@@ -73,6 +65,22 @@ export default class Cpn_TextArea extends React.Component {
 
     fn_updateThisState = () => { //一旦监听到财务专家更新了state对象, 就执行本回调函数
         this.setState(objStore.getState())
+    }
+
+    fn_sendTextareaContent_ByAxios = (value) => {
+        console.log('执行fn_sendTextareaContent_ByAxios()函数');
+        axios.get('#',
+            {
+                params:
+                    {textareaContent: value}
+            })
+            .then(res=>{
+                console.log(res.data);
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+
     }
 
 }
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const {parse} = require('url')
 const next = require('next')
 const url = require('url')
 const fs = require('fs')
-let myFunc_mongoose_connectKollins = require('./store/myFunc_mongoose_connectKollins')
+// let myFunc_mongoose_connectKollins = require('./store/myFunc_mongoose_connectKollins')
 
 //----------------------------------------
-
-let url_DbTest = "mongodb://localhost/test"
-//连接test数据库
-myFunc_mongoose_connectKollins.fn_连接数据库(url_DbTest) //拿到 collection reactState 的 model对象
-
+//
+// let url_DbTest = "mongodb://localhost/test"
+// //连接test数据库
+// myFunc_mongoose_connectKollins.fn_连接数据库(url_DbTest) //拿到 collection reactState 的 model对象
+//
 
 //----------------------------------------
 
@@ -30,22 +30,27 @@ app.prepare().then(() => {
             console.log('哈哈哈');
             app.render(req, res, '/', query)
             let objQuery = url.parse(req.url, true).query //获取发送给"/"这个url地址的 GET请求内容
-            console.log(objQuery);
+            console.log('objQuery ===>',objQuery);
             /*
             注意, 由于这里没有用express,而是node.js, 所以我们获取get请求的内容时,
             不能用express的req.query, 而要用node.js的获取get请求的方法, 即: url.parse(req.url, true).query
              */
 
             //react中多行文本框的数据, 在get请求的area_StrSelected字段中.
-            console.log(objQuery.area_StrSelected);
+            console.log('objQuery.textareaContent ===>',objQuery.area_StrSelected);
 
-            fn_写入文件('../01_myLearn/辞典kollins/柯林斯选.txt', objQuery.area_StrSelected + '\r\n---------------------\r\n\r\n')
+            console.log('+++++');
+            fn_写入文件('../01_myLearn/辞典kollins/柯林斯选.txt', objQuery.textareaContent + '\r\n---------------------\r\n\r\n')
 
 
             // 使用数据库, 查看kollins collection 里面的所有document. 下面代码亲测是成功的!
             // 注意, 更改了本server.js的代码后, 需要每次重新运行 npm run dev 才能看到效果
-            myFunc_mongoose_connectKollins.fn_findAllDoc(
-                myFunc_mongoose_connectKollins.ModelReactState, {}, ["index_wordCurrentFind", "index_start", "index_end"])
+            //     myFunc_mongoose_connectKollins.fn_findAllDoc(
+            //         myFunc_mongoose_connectKollins.ModelReactState, {}, ["index_wordCurrentFind", "index_start", "index_end"])
+            // }
+            //
+            // if (pathname === '/sendTextareaContent_ByAxios') {
+            //     console.log('服务器检测到对 /sendTextareaContent_ByAxios的请求');
         }
 
 
